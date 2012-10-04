6cb1c844c047626fc4b43f2f76fd34eafdbc30ea
Rooslunn <rooslunn@gmail.com>
2012-10-04 11:49:25 +0300

    update

--- application/controllers/task1.php
+++ application/controllers/task1.php
@@ -5,6 +5,6 @@ class Task1_Controller extends Base_Controller {
     public $restful = true;
 
     public function get_main() {
-        echo '123';
+        return Response::make(123, 200, array('Content-Type: text/plain'));
     }
 }
