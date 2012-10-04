bf5d934cea57298d7dc065d50e390946229c574c
Rooslunn <rooslunn@gmail.com>
2012-10-04 11:56:21 +0300

    update

--- application/controllers/task1.php
+++ application/controllers/task1.php
@@ -4,7 +4,13 @@ class Task1_Controller extends Base_Controller {
     
     public $restful = true;
 
+    private function eval_route() {
+        return URI::current();
+    }
+
     public function get_main() {
-        return Response::make(123, 200, array('Content-Type' => 'text/plain'));
+        $resp = Response::make($this->eval_route());
+        $resp.header('Content-Type', 'text/plain');
+        return $resp;
     }
 }
